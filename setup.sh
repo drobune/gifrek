5612b1080e214e2bd4da8fd089162301a99ac40e
drobune <s.yuichi227@gmail.com>
2017-01-14 20:03:02 +0000

    Use tmp dir to build xselrect

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-wget -qO- https://github.com/lolilolicon/xrectsel/archive/0.3.2.tar.gz | tar xvz -C ./lib
-cd ./lib/xrectsel-0.3.2 && ./bootstrap && ./configure && make && sudo make install
+tempdir=$(mktemp -d)
+wget -qO- https://github.com/lolilolicon/xrectsel/archive/0.3.2.tar.gz | tar xvz -C ${tempdir}
+cd "${tempdir}/xrectsel-0.3.2" && echo $(pwd) #./bootstrap && ./configure && make && sudo make install
 
 
